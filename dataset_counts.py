c6b7943849ebfc67d4edfc6fdd4b8743467fd0bc
Monika <monmonberry@users.noreply.github.com>
2016-07-11 15:04:58 +1000

    A simple feature class counting script using ArcPy

---
+++ dataset_counts.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 14:21:55 2016
+
+@author: Monika
+"""
+
+import arcpy
+
+
+a = arcpy.GetCount_management(raw_input("Please enter the Feature Class Location below\n-->"))
+
+print a
+
